6af175cabe1e272593c415265962f709d8accf7f
rbastiansch <rafaelbastiansch@gmail.com>
2023-04-08 16:53:23 -0300

    feat: update dependencies

--- app/Controllers/Http/SentencesController.ts
+++ app/Controllers/Http/SentencesController.ts
@@ -3,24 +3,32 @@ import Service from 'App/Services/SentencesService'
 import { DateTime } from 'luxon'
 
 export default class SentencesController {
-  public async index() {
-    return await Sentence.all()
+  public async index({ auth }) {
+    await auth.use('api').authenticate()
+
+    const { id } = auth.use('api').user!
+
+    return await Sentence.query().where({ userId: id })
   }
 
-  public async store({ request, response }) {
-    const sentence = new Sentence()
+  public async store({ auth, request, response }) {
+    await auth.use('api').authenticate()
+    const { id } = auth.use('api').user!
 
     const sentenceWithTimeToRepeat = await new Service().addSentenceCurrentTimeToRepeat(
       request.all()
     )
-    const result = await sentence.fill({ ...sentenceWithTimeToRepeat }).save()
+    const result = await new Sentence().fill({ ...sentenceWithTimeToRepeat, userId: id }).save()
 
     response.send(result)
   }
 
-  public async update({ request, response }) {
+  public async update({ auth, request, response }) {
+    await auth.use('api').authenticate()
+    const { id: userId } = auth.use('api').user!
+
     const { id } = request.params()
-    const sentence = await Sentence.findOrFail(id)
+    const sentence = await Sentence.query().where({ id, userId }).firstOrFail()
 
     const updatedSentence = new Service().updateSentenceBasedOnDate({
       ...request.all(),
